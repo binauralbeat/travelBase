9e33a303a8d820dc382cbb9f9572f027e406ae66
binauralbeat <binauralbeat001@gmail.com>
2018-04-15 14:00:51 -0500

    local storage up and running

--- main.js
+++ main.js
@@ -7,38 +7,46 @@ TravelDatabase.cities = []
 // Add cities to the student table in the student database
 TravelDatabase.cities.push(
    {
-       city: “Nehalem Bay, OR”,
-       year: “2003”,
-       image: “src=”
-       "top five":
+       city: "Nehalem Bay OR",
+       year: "2003",
+       image: "",
+       attracions :""
    },
    {
-       city: “”,
-       year: “”,
-       image: “F”
+       city: "",
+       year: "",
+       image: "",
+       attracions :""
+
    },
    {
-       city: “”,
-       year: “”,
-       image: “”
+       city: "",
+       year: "",
+       image: "",
+       attracions :""
+
    },
    {
-       city: “”,
-       year: “”,
-       image: “”
+       city: "",
+       year: "",
+       image: "",
+       attracions :""
    },
    {
-       city: “”,
-       year: “”,
-       image: “F”
+       city: "",
+       year: "",
+       image: "",
+       attracions :""
    },
    {
-       city: “”,
-       year: “”,
-       image: “”
+       city: "",
+       year: "",
+       image: "",
+       attracions :""
+       
    }
 )
 
 localStorage.setItem(
     "TravelDatabase",
-    JSON.stringify(TravelDatabase)
+    JSON.stringify(TravelDatabase))
